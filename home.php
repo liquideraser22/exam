26a366cf5715dd7c499953ca4c482269d61b7fd1
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-02-12 23:34:46 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,41 @@
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://www.fandango.com/rss/top10boxoffice.rss");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("item");
+?>
+    <br/> <br/>
+   <div class="brand"> OUR TOP 10 MOVIES FOR THE WEEK </div>
+
+<?php 
+ foreach( $content as $data )
+ {
+   $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+   $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+   $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+   $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
+   
+   ?>
+
+    <html>
+    <head> <title> MOVIETASTIC|TOP 10 PICKS FOR THE WEEK </title> 
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Kanit:800i|Oswald|Teko:600&display=swap" rel="stylesheet">
+    </head>
+    <body> 
+    
+                   <a href="<?php echo $link;?>" target="blank"> <div class="article-title"> <?php echo $title;?> </div>  </a>
+                                <ul class="article-content">
+                                  <div class="article-description"> <?php echo $description; ?> </div> <br/>
+                                    <li> <b> Link: </b> <a href="<?php echo $link;?>" target="blank"> <?php echo $link;?>  </a> </li>
+                                    <li> <b> Author: </b> <?php echo $author; ?></li>
+                                </li>
+                                </ul>
+                            </li>
+ <?php
+ }
+?>
+
+</body>
+</html>
+
