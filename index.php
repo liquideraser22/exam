81e6ae5e16e9da9db6e564d43669be241a0e5bf4
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-02-12 23:35:14 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php 
+$loadnav = (isset($_GET['loadnav']) && $_GET['loadnav'] != '') ? $_GET['loadnav']: '';
+?>
+<html> 
+    <head>
+        <title>  MOVIETASTIC </title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="icon" href="img/favicon.png" type="image/gif">
+        <link href="https://fonts.googleapis.com/css?family=Bangers&display=swap" rel="stylesheet">
+    </head>
+    <body>
+    <div class="wrapper">
+
+        <div class="indexheader"> <div class="brand"> MOVIETASTIC </div> </div>
+                        
+                        <div class="nav">
+                        <ul class="nav-menu"> 
+                        <li> <a href="index.php?loadnav=home"> HOME </a> </li>
+                        <li> <a href="index.php?loadnav=newmovies"> NEW MOVIES </a> </li>
+                        <li> <a href="index.php?loadnav=weekenders"> WEEKENDERS  </a> </li>
+                        <li> <a href="index.php?loadnav=spookynight"> SPOOKYNIGHT  </a> </li>
+                        <li> <a href="index.php?loadnav=datenight"> DATE NIGHT </a> </li>
+                        </ul>
+                        </div>
+
+                        <div class="content">
+                        <?php 
+                        switch($loadnav){
+                            case 'newmovies':
+                            require_once 'newmovies.php';
+                            break;
+                            case 'weekenders':
+                            require_once 'weekenders.php';
+                            break;
+                            case 'spookynight':
+                            require_once 'spookynight.php';
+                            break;
+                            case 'datenight':
+                            require_once 'datenight.php';
+                            break;
+                            default:
+                            require_once 'home.php';
+                            break;
+                        }
+                        ?>
+                        </div>
+
+                        <div id="footer">
+                    
+                        <div class="indexheader"> <div class="brand"> MOVIETASTIC™ </div>
+                        </div>
+    </div>
+    </body>
+</html>
