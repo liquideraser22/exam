875bfed2a43fff3584a8588a901c54f6cbc981ab
liquideraser22 <60122559+liquideraser22@users.noreply.github.com>
2020-02-12 23:35:36 +0800

    Create newmovies.php

---
+++ newmovies.php
@@ -0,0 +1,42 @@
+<?php
+ $domOBJ = new DOMDocument();
+ $domOBJ->load("https://www.fandango.com/rss/newmovies.rss");//XML page URL
+ 
+ $content = $domOBJ->getElementsByTagName("item");
+?>
+    <br/> <br/>
+   <div class="brand"> UPCOMING MOVIES </div> 
+                     
+<?php 
+ foreach( $content as $data )
+ {
+   $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
+   $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+   $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+   $author = $data->getElementsByTagName("author")->item(0)->nodeValue;
+   $pubDate = $data->getElementsByTagName("pubDate")->item(0)->nodeValue;
+
+   ?>
+
+    <html>
+    <head> <title> MOVIETASTIC|NEW MOVIES </title> 
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Kanit:800i|Oswald|Teko:600&display=swap" rel="stylesheet">
+    </head>
+    <body> 
+    
+                   <a href="<?php echo $link;?>" target="blank"> <div class="article-title"> <?php echo $title;?> </div>  </a>
+                                <ul class="article-content">
+                                  <div class="article-description"> <?php echo $description; ?> </div> <br/>
+                                    <li> <b> Link: </b> <a href="<?php echo $link;?>" target="blank"> <?php echo $link;?>  </a> </li>
+                                    <li> <b> Published Date:</b> <?php echo $pubDate ?></li>
+                                    <li> <b> Author: </b> <?php echo $author; ?></li>
+                                </li>
+                                </ul>
+                            </li>
+ <?php
+ }
+?>
+
+</body>
+</html>
